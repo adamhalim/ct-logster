976aa53016f41f1d5437fbe8e8891b20b47f8e86
Adam Abdel-Halim <adaab301@student.liu.se>
2021-03-23 15:57:45 +0100

    Now pushes entire structo to DB
    Also added CT log url to struct

--- main.go
+++ main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	"github.com/CaliDog/certstream-go"
+	"context"
 	"fmt"
 	"log"
+	"os"
 	"strings"
+	"time"
+
+	"github.com/CaliDog/certstream-go"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"os"
-	"context"
-	"time"
+
 )
 
 type CertInfo struct {
-	CertIndex		int32
-	SerialNumber	string
-	Domain			[]string
-	OCSP			string
-	CRL				[]string
+	CertIndex    int		`bson:"certIndex"`
+	SerialNumber string		`bson:"serialNumber"`
+	Domain       []string	`bson:"domains"`
+	OCSP         string		`bson:"OCSP,omitempty"`
+	CRL          string		`bson:"CRL,omitempty"`
+	CTlog		 string		`bson:"ctLog"`
 }
 
-var dbUsername, dbPassword, dbIp, dbPort string;
 
 // Loads .env file
 func init() {
@@ -37,32 +38,24 @@ func init() {
 	dbPort = os.Getenv("PORT")
 }
 
-
 // Makes one insertion into MongoDB
 func insertIntoDB(client mongo.Client, ctx context.Context, cancel context.CancelFunc,
-	certIndex int, serialNumber string, domain []string, OCSP string, CRL string) {
+	cert CertInfo) {
 
-	collection := client.Database("dev").Collection("certificates")
+	collection := client.Database("dev").Collection("certTestTwo")
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	//Actual insert to MongoDB. Could possibly be done in batches for better performance
-	_, err := collection.InsertOne(ctx, bson.D{
-		{"certIndex", certIndex},
-		{"serialNumber", serialNumber},
-		{"Domain", domain},
-		{"OCSP", OCSP },
-		{"CRL", CRL},
-		{"Time", time.Now().Hour()},
-	})
-	if (err != nil) {
+	_, err := collection.InsertOne(ctx, cert)
+	if err != nil {
 		fmt.Print("Error inserting.")
 	}
 }
 
 func main() {
 	// The false flag specifies that we want heartbeat messages.
-	stream, errStream := certstream.CertStreamEventStream(false);
+	stream, errStream := certstream.CertStreamEventStream(false)
 
 	// Establish connection to MongoDB
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -77,75 +70,96 @@ func main() {
 		}
 	}()
 
-
 	ctx, cancel = context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	err = client.Ping(ctx, readpref.Primary())
 
 	fmt.Println("Connected to MongoDB Server: " + dbIp + ".")
 
-
 	for {
 		//For more informaiton please see: https://pkg.go.dev/github.com/jmoiron/jsonq
 		select {
 		case jq := <-stream:
-			_, err := jq.String("message_type");
-
-			if err != nil {	log.Printf("Error decoding jq string"); }
-			
-			// Here, we grab all fields from the JQ response
-			CertInfo, err := jq.Int("data", "cert_index");
-			if err != nil {	log.Printf("Error decoding jq cert index."); }
-
-			SerialNumber, err := jq.String("data", "leaf_cert","serial_number")
-			if err != nil {	log.Printf("Error decoding jq serial number."); }
-
-			Domain, err := jq.ArrayOfStrings("data", "leaf_cert", "all_domains");
-			if err != nil {	log.Printf("Error decoding jq domains."); }
-
-			OCSP, err := jq.String("data", "leaf_cert", "extensions", "authorityInfoAccess");
+			_, err := jq.String("message_type")
 
 			if err != nil {
-				log.Printf("Error decoding jq OCSP.");
+				log.Printf("Error decoding jq string")
+			}
+
+			// Here, we grab all fields from the JQ response
+			CertIndex, err := jq.Int("data", "cert_index")
+			if err != nil {
+				log.Printf("Error decoding jq cert index.")
+			}
+
+			SerialNumber, err := jq.String("data", "leaf_cert", "serial_number")
+			if err != nil {
+				log.Printf("Error decoding jq serial number.")
+			}
+
+			Domain, err := jq.ArrayOfStrings("data", "leaf_cert", "all_domains")
+			if err != nil {
+				log.Printf("Error decoding jq domains.")
+			}
+
+			OCSP, err := jq.String("data", "leaf_cert", "extensions", "authorityInfoAccess")
+
+			if err != nil {
+				log.Printf("Error decoding jq OCSP.")
 			} else {
-				if(strings.Contains(OCSP, "OCSP - URI:")) {
-					tempStr := strings.Split(OCSP, "OCSP - URI:");
-					if (len(tempStr) > 1) {
-						if(strings.Contains(tempStr[1], "CA Issuers - URI:")) {
-							OCSP = (strings.Split(tempStr[1], "CA Issuers - URI:"))[0];
-							OCSP = strings.Trim(OCSP, "\n");
+				if strings.Contains(OCSP, "OCSP - URI:") {
+					tempStr := strings.Split(OCSP, "OCSP - URI:")
+					if len(tempStr) > 1 {
+						if strings.Contains(tempStr[1], "CA Issuers - URI:") {
+							OCSP = (strings.Split(tempStr[1], "CA Issuers - URI:"))[0]
+							OCSP = strings.Trim(OCSP, "\n")
 						} else {
-							OCSP = tempStr[1];
-							OCSP = strings.Trim(OCSP, "\n");
+							OCSP = tempStr[1]
+							OCSP = strings.Trim(OCSP, "\n")
 						}
 					}
 				} else {
 					fmt.Printf("Error: No OCSP URI found.")
-				}	
-			}			
+				}
+			}
 
 			//Error prone due to inconsistent certificate format.
 			CRL, err := jq.String("data", "leaf_cert", "extensions", "crlDistributionPoints")
 			if err != nil {
-				log.Printf("Error decoding jq CRL.");
+				log.Printf("Error decoding jq CRL.")
 			} else {
-				CRL = strings.Split((strings.Split(CRL, "URI:")[1]), "\n")[0];
+				CRL = strings.Split((strings.Split(CRL, "URI:")[1]), "\n")[0]
 			}
 
-			//ctx and cancel given from beginning of main.
-			insertIntoDB(*client, ctx, cancel, CertInfo, SerialNumber, Domain, OCSP, CRL);
+			CTlog, err := jq.String("data", "source", "url")
+			if err != nil {
+				log.Print("No CT log URL found.")
+			}
 
+			// Fill struct with data
+			cert := CertInfo{
+				CertIndex: CertIndex,
+				SerialNumber: SerialNumber,
+				Domain: Domain,
+				OCSP: OCSP,
+				CRL: CRL,
+				CTlog: CTlog,
+			} 
+			
+			//ctx and cancel given from beginning of main.
+			insertIntoDB(*client, ctx, cancel, cert);
 
 			//dev-prints:
-			fmt.Printf("Cert index: %d\n", CertInfo);
-			fmt.Printf("Serial number: %s\n", SerialNumber);
-			fmt.Printf("Domain: %s\n", Domain);
-			fmt.Printf("CRL URL: %s\n", CRL);
-			fmt.Printf("OCSP URL: %s\n", OCSP);
-			fmt.Printf("\n");
+			fmt.Printf("Cert index: %d\n", CertIndex)
+			fmt.Printf("Serial number: %s\n", SerialNumber)
+			fmt.Printf("Domain: %s\n", Domain)
+			fmt.Printf("CRL URL: %s\n", CRL)
+			fmt.Printf("OCSP URL: %s\n", OCSP)
+			fmt.Printf("CT log URL: %s\n", CTlog)
+			fmt.Printf("\n")
 
 		case err := <-errStream:
-			log.Printf(err.Error());
+			log.Printf(err.Error())
 		}
 	}
 }
