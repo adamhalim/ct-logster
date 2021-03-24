9c967ba830c4279cff58ecb72ed8e83a52209989
Max Danielsson <maxda362@student.liu.se>
2021-03-24 22:48:24 +0100

    Created basic functionality for fetching blocks
    Currently doesn't return anything. Can easily be modified to return all
    the entries for a certain block.

--- logster/DBhandler.go
+++ logster/DBhandler.go
@@ -5,19 +5,19 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	//"go.mongodb.org/mongo-driver/mongo/readpref"
 
 	"context"
 	"time"
 	"fmt"
 	"log"
 	"os"
-	"sync"
+	//"sync"
 )
 
 
 var dbUsername, dbPassword, dbIp, dbPort string
-const database, collection = "dev", "certTestTwo"
+const database, collection = "dev", "certTest"
 
 // Loads .env file
 func init() {
@@ -33,6 +33,7 @@ func init() {
 
 func main() {
 	IterateAllCerts()
+	IterateBlock(11)
 }
 
 // This will iterate though all certs in 
@@ -101,3 +102,64 @@ func IterateAllCerts() {
 	}
 }
 
+func IterateBlock(blockTime int){
+	// Establish connection to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	uri := "mongodb://" + dbIp + ":" + dbPort
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+
+	//disconnects the db when exiting main.
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+
+	// Pass these options to the Find method
+	findOptions := options.Find()
+	findOptions.SetLimit(20)
+
+	// Here's an array in which you can store the decoded documents
+	var res []*CertInfo
+
+	col := client.Database(database).Collection(collection)
+
+	// Passing bson.D{{}} as the filter matches all documents in the collection
+	cur, err := col.Find(context.TODO(), bson.D{{"Time", blockTime}}, findOptions)
+	if err != nil {
+		fmt.Println("Error when collecting")
+	}
+
+	// Finding multiple documents returns a cursor
+	// Iterating through the cursor allows us to decode documents one at a time
+	for cur.Next(context.TODO()) {
+
+		// create a value into which the single document can be decoded
+		var elem CertInfo
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		res = append(res, &elem)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the cursor once finished
+	cur.Close(context.TODO())
+
+	fmt.Printf("Found multiple documents (array of pointers): %+v\n", res)
+
+	for _,entry := range res{
+		fmt.Println(entry)
+	}
+}
