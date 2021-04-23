a570cc89171ff54feb65c1cf901521c8223e559f
Max Danielsson <maxda362@student.liu.se>
2021-04-23 14:09:36 +0200

    Added client timeout to ocsp Post/Get request
    Added timeout of 15 seconds to reduce reasource "hoarding" behavior in
    slow responses. Might increase or decrease time depending on results.
    
    Done by creating http client and giving it timeout attribute.

--- revocado/ocspRevo.go
+++ revocado/ocspRevo.go
@@ -15,6 +15,7 @@ import (
 	"crypto"
 	"math/rand"
 	"strconv"
+	"time"
 )
 
 
@@ -30,8 +31,8 @@ func GetOCSP(url string, issuer *x509.Certificate, cert *x509.Certificate) (stat
 	} else if ocspResp.Status == ocsp.Unknown {
 		return "Unknown", nil
 	} else if ocspResp.Status == ocsp.Revoked {
-		fmt.Println(ocspResp.RevocationReason)
-		return "Revoked", nil
+		reason := "Revoked:" + strconv.Itoa(ocspResp.RevocationReason)
+		return reason, nil
 	} else {
 		return "Unexcpected", nil
 	}
@@ -43,15 +44,21 @@ func GetOCSP(url string, issuer *x509.Certificate, cert *x509.Certificate) (stat
 func sendOCSPRequest(url string, req []byte, issuer *x509.Certificate) (ocspResponse *ocsp.Response, err error) {
     var resp *http.Response
 
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
     if len(req) > 256 {
         buf := bytes.NewBuffer(req)
-        resp, err = http.Post(url, "application/ocsp-request", buf)
+        resp, err = client.Post(url, "application/ocsp-request", buf)
     } else {
         reqURL := url + "/" + base64.StdEncoding.EncodeToString(req)
-        resp, err = http.Get(reqURL)
+        resp, err = client.Get(reqURL)
     }
 
-    if err != nil {
+    defer resp.Body.Close()
+
+	if err != nil {
         return nil, err
     }
 
@@ -65,7 +72,7 @@ func sendOCSPRequest(url string, req []byte, issuer *x509.Certificate) (ocspResp
     if err != nil {
         return nil, errors.New("Could not read body")
     }
-    resp.Body.Close()
+
 
     return ocsp.ParseResponse(body, issuer)
 }
